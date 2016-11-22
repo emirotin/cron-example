d7dde0c43a70a3375121de9fa339e81f374799d0
Eugene Mirotin <emirotin@gmail.com>
2016-11-22 22:55:44 +0300

    export env vars

--- job.sh
+++ job.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export $(xargs -0 -n1 < /proc/1/environ)
+
 # /proc/1/fd/1 is systemd's stdout, and therefore appears in the resin logs.
 echo "Hello from [$RESIN_DEVICE_UUID]" >/proc/1/fd/1
